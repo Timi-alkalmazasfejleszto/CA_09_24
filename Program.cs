1d91e3abc85725771245b8358d424d3b09e16736
Timi-alkalmazasfejleszto <kantor.timea@verebelyszki.hu>
2026-09-24 11:34:54 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,13 +3,12 @@
 Console.Write("Enter your name: ");
 string name = Console.ReadLine() ?? throw new Exception("cannot be null");
 
-
 string hw = $"Hello, {name}!";
 
 Console.Write("How many times? ");
 int num=int.Parse(Console.ReadLine());
 
-for (int i=0, i<num; i++){
+for (int i=0; i<num; i++){
     foreach (var c in hw) 
     {
         Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
