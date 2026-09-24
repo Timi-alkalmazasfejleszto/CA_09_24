9df207920e70da7362f1d5b039ffd0454cdbd535
Timi-alkalmazasfejleszto <kantor.timea@verebelyszki.hu>
2026-09-24 11:30:36 +0200

    Personalize greeting and prompt for repetition count
    Updated greeting message to include user name and added input for repetition count.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ Console.Write("Enter your name: ");
 string name = Console.ReadLine() ?? throw new Exception("cannot be null");
 
 
-string hw = "Hello, World!";
+string hw = $"Hello, {name}!";
 
+Console.Write("How many times?");
+int num=int.Parse(Console.ReadLine);
 foreach (var c in hw) 
 {
     Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
