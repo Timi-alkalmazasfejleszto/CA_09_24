5f4b49ac7957f1e78601a5035183dbc9cfc9f97b
Kántor Tímea Zsófi <kantor.timea@verebelyszki.hu>
2026-09-24 11:26:57 +0200

    add "name", remove "world"

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿
+
+Console.Write("Enter your name: ");
+string name = Console.ReadLine() ?? throw new Exception("cannot be null");
+
+
 string hw = "Hello, World!";
 
 foreach (var c in hw) 
@@ -6,3 +11,5 @@ foreach (var c in hw)
     Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
     Console.Write(c);
 }
+
+while (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
